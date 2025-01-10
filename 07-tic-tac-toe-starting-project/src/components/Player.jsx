3383cde2debded1358e0d81d27adb80e94467eb4
Tancre <outisisout@gmail.com>
2025-01-10 12:30:27 +0100

    handler edit btn

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 export default function Player({name, symbol}) {
-  return (
+  const [ isEditing, setIsEditing ] = useState(false);
+  
+  const handleClick = () => setIsEditing(true);
+
+  let playerName = !isEditing ? <span className="player-name">{name}</span> : <input />;
+  
+  return ( 
     <li>
-      <span className="player">
-        <span className="player-name">{name}</span>
+      <span className="player"> 
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleClick}>Edit</button>
     </li>
   )
 }
