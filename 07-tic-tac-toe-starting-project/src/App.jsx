4279fc8d3385380109a4e1ea6a2beb6ef0bf1285
Tancre <outisisout@gmail.com>
2025-01-10 12:29:04 +0100

    typo X to O

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <div id="game-container">
         <ol id="players">
           <Player name={"Player 1"} symbol={"X"} />
-          <Player name={"Player 2"} symbol={"X"} />
+          <Player name={"Player 2"} symbol={"O"} />
         </ol>
       </div>
     </main>
